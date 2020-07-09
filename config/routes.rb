18ddb3e7a2d38d22354e9293958964fc0d71ccce
panhara <titpanhara@gmail.com>
2020-07-09 11:14:44 +0700

    Changes to be committed: 	modified:   config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  
+  # Serve websocket cable requests in-process
+  mount ActionCable.server => '/cable'
   root "messages#index"
   resources :messages
 
-  # Serve websocket cable requests in-process
-  mount ActionCable.server => '/cable'
 end
