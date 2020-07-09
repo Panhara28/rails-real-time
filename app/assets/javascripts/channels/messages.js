6149e1cd49126d67d8417b6200fac6d0cf4aafa4
panhara <titpanhara@gmail.com>
2020-07-09 13:56:40 +0700

    Changes to be committed: 	modified:   app/assets/javascripts/channels/messages.js

--- app/assets/javascripts/channels/messages.js
+++ app/assets/javascripts/channels/messages.js
@@ -7,11 +7,15 @@ this.App = {};
 App.cable = ActionCable.createConsumer("/cable");
 
 App.messages = App.cable.subscriptions.create('MessagesChannel', {
+
   received: function(data) {
+    console.log('Received');
     $("#messages").removeClass('hidden')
     return $('#messages').append(this.renderMessage(data));
   },
   renderMessage: function(data) {
+    console.log('Data Sent');
+
     return "<p>" + data.message + "</p>";
   }
 });
